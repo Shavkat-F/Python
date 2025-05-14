2e58659101c9c4b89844f78d0b476c86e581ddf5
Shavkat-F <fudjiyama@icloud.com>
2025-05-14 12:27:04 +0500

    Create hwpy1.py

---
+++ lesoon-1/homework/hwpy1.py
@@ -0,0 +1,33 @@
+side = float(input("Enter the side of the square: "))
+perimeter = 4 * side
+area = side ** 2
+
+print("Perimeter:", perimeter)
+print("Area:", area)
+
+
+import math
+
+diameter = float(input("Enter the diameter of the circle: "))
+circumference = math.pi * diameter
+
+print("Circumference:", circumference)
+
+a = float(input("Enter first number (a): "))
+b = float(input("Enter second number (b): "))
+mean = (a + b) / 2
+
+print("Mean:", mean)
+
+a = float(input("Enter first number (a): "))
+b = float(input("Enter second number (b): "))
+
+sum_result = a + b
+product = a * b
+square_a = a ** 2
+square_b = b ** 2
+
+print("Sum:", sum_result)
+print("Product:", product)
+print("Square of a:", square_a)
+print("Square of b:", square_b)
